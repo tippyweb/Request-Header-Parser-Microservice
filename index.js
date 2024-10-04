3782cb718f43c7755086d2b7f36b40fbb33d3720
Deborah Sanders <dsanders703@gmail.com>
2024-10-04 09:15:10 +0000

    Initial commit

--- index.js
+++ index.js
@@ -24,6 +24,22 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+
+// Request Header Parser Microservice
+app.get('/api/whoami', (req, res) => {
+  const ipaddress = req.headers['x-forwarded-for'];
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+  
+  res.json({
+    "ipaddress": ipaddress,
+    "language": language,
+    "software": software
+  });
+  
+  });
+
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
